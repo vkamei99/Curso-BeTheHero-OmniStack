44d4bfc0dc9bdf1fa9c2cb4e7ad9e0055796697c
Viktor Kamei Mota <vikmotakamei@gmail.com>
2023-08-19 19:59:59 -0300

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/users/:id', (request, response) =>{
+
+    const QueryParams = request.query;
+    const RouteParams = request.params;
+    const BodyParams = request.body;
+
+    console.log(RouteParams);
+    console.log(QueryParams);
+    console.log(BodyParams);
+
+    return response.json({
+        nome : "Kaio",
+        sobrenome : "Kamei",
+        aniversário : "26/11"
+    })
+})
+
+
+app.listen(3333);
